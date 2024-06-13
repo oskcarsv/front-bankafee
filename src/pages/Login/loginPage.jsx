12ab368fcac1c9878c52691c523e7133b9d20df5
amendez2019362 <amendez-2019362@kinal.edu.gt>
2024-06-12 21:56:25 -0600

    feat: Add login page to routes

---
+++ src/pages/Login/loginPage.jsx
@@ -0,0 +1,7 @@
+export const loginPage = () => {
+    return (
+        <div>
+            <h1>Login Page</h1>
+        </div>
+    )
+};
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,11 +1,13 @@
 import { LandingPage } from "./pages/landingPage/landingPage.jsx";
 import { LandingPageAboutUs } from "./pages/landingPageAboutUs/landingPageAboutUs.jsx";
 import { LandingPageService } from "./pages/landingPageService/landingPageService.jsx";
+import { loginPage } from "./pages/Login/loginPage.jsx";
 
 const routes = [
     { path: "/", element: <LandingPage />},
     { path: "/aboutUs", element: <LandingPageAboutUs />},
     { path: "/service", element: <LandingPageService />},
+    { path: "/login", element: <loginPage />}
 ];
 
 export default routes;
