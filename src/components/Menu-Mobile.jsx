c47203ec180a6f7c924fa612342d8f4405264a0f
Jose David Soto Puac <jsoto-2019315@kinal.edu.gt>
2024-06-18 20:36:15 -0600

    chore: Add mobile menu link to home page in MenuMobile component

--- src/components/Menu-Mobile.jsx
+++ src/components/Menu-Mobile.jsx
@@ -1,8 +1,10 @@
 import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
 import cancelIcon from "../assets/iconLanding/Cancel-Icon.svg";
 import signInIcon from "../assets/iconLanding/SignIn-Icon.svg";
 import aboutUsIcon from "../assets/iconLanding/AboutUs-Icon.svg";
 import servicesIcon from "../assets/iconLanding/Services-Icon.svg";
+import homeIcon from "../assets/iconAbout/HomeIconMobile.svg";
 import "../styles/landingPagesCss/menu-mobile.css";
 
 export const MenuMobile = ({ onClose }) => {
@@ -12,6 +14,8 @@ export const MenuMobile = ({ onClose }) => {
       document.body.classList.remove("no-scroll");
     };
   }, []);
+
+  const currentPath = window.location.pathname;
 
   return (
     <div className="menu-mobile">
@@ -26,13 +30,18 @@ export const MenuMobile = ({ onClose }) => {
           <img src={signInIcon} alt="Sign In Icon" className="sign-in-icon" />
           <p className="menu-mobile-p">Sign In</p>
         </li>
-        <li className="menu-mobile-li">
-          <img
-            src={aboutUsIcon}
-            alt="About Us Icon"
-            className="about-us-icon"
-          />
-          <p className="menu-mobile-p">About us</p>
+        <li>
+          {currentPath === "/aboutUs" ? (
+            <Link to="/" className="menu-mobile-li">
+              <img src={homeIcon} alt="Home Icon" />
+              <p className="menu-mobile-p">Home</p>
+            </Link>
+          ) : (
+            <Link to='/aboutUs' className="menu-mobile-li">
+              <img src={aboutUsIcon} alt="About Us Icon" className="about-us-icon" />
+              <p className="menu-mobile-p">About us</p>
+            </Link>
+          )}
         </li>
         <li className="menu-mobile-li">
           <img
@@ -42,7 +51,7 @@ export const MenuMobile = ({ onClose }) => {
           />
           <p className="menu-mobile-p">Services</p>
         </li>
-      </ul>
-    </div>
+      </ul >
+    </div >
   );
 };
