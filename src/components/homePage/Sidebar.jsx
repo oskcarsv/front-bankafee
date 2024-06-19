0e89cbb04ada65eeeb30edc03f4f8060cf38720f
amendez2019362 <amendez-2019362@kinal.edu.gt>
2024-06-18 23:01:34 -0600

    chore: Update home page styles and components, add message and list sections

--- src/components/homePage/Sidebar.jsx
+++ src/components/homePage/Sidebar.jsx
@@ -1,6 +1,6 @@
-import  logo  from '../../assets/iconLanding/logo.svg';
-import  perfil  from '../../assets/iconSidebar/perfil.svg';
-import editUser  from '../../assets/iconSidebar/editUser.svg';
+import logo from '../../assets/iconLanding/logo.svg';
+import perfil from '../../assets/iconSidebar/perfil.svg';
+import editUser from '../../assets/iconSidebar/editUser.svg';
 import createAccount from '../../assets/iconSidebar/createAccount.svg';
 import history from '../../assets/iconSidebar/history.svg';
 import logout from '../../assets/iconSidebar/logout.svg';
@@ -14,15 +14,15 @@ export const Sidebar = () => {
             <img src={perfil} alt="img" className='img-perfil-sidebar' />
             <h1 className='name-profile'>Angel Mendez <img src={editUser} alt="icon" className='icon-edit-user' /></h1>
             <h2 className='email-profile'>amendez@gmail.com</h2>
-            <ul className="sidebar-menu">   
+            <ul className="sidebar-menu">
                 <li className="sidebar-menu-item">
-                    <button className="sidebar-menu-button">Create <img src={createAccount} alt="icon" className='icon-create-account'/></button>
+                    <button className="sidebar-menu-button">Create <img src={createAccount} alt="icon" className='icon-create-account' /></button>
                 </li>
                 <li className="sidebar-menu-item">
                     <button className="sidebar-menu-button">History <img src={history} alt="icon" className='icon-history' /></button>
                 </li>
                 <li className="sidebar-menu-item-">
-                    <button className="sidebar-menu-button-logout"><img src={logout} alt="img" className='img-sidebar-logout'/></button>
+                    <button className="sidebar-menu-button-logout"><img src={logout} alt="img" className='img-sidebar-logout' /></button>
                 </li>
             </ul>
         </div>
