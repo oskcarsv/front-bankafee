d94041098b38a87b93cb194be6c4eb65a6561bfc
amendez2019362 <amendez-2019362@kinal.edu.gt>
2024-06-25 12:41:14 -0600

    feat: Update category product layout and styles

--- src/components/productHomePage/CategoryProduct.jsx
+++ src/components/productHomePage/CategoryProduct.jsx
@@ -11,16 +11,20 @@ export const CategoryProduct = () => {
         <section className='section-category-product'>
             <div className='content-category'>
                 <div className='card-category'>
-                    <p className='title-category'>Category Electrodomestic</p>
                     <Link><img src={categoryElectrodomestic} alt="img" className='img-category' /></Link>
+                    <p className='title-category'>Category Electrodomestic</p>
                 </div>
                 <div className='card-category'>
-                    <p className='title-category'>Category Food</p>
                     <Link><img src={categoryFood} alt="img" className='img-category' /></Link>
+                    <p className='title-category'>Category Food</p>
                 </div>
                 <div className='card-category'>
-                    <p className='title-category'> Category Cars</p>
                     <Link><img src={categoryCars} alt="img" className='img-category' /></Link>
+                    <p className='title-category'> Category Cars</p>
+                </div>
+                <div className='card-category'>
+                    <Link><img src={categoryCars} alt="img" className='img-category' /></Link>
+                    <p className='title-category'> Category Cars</p>
                 </div>
             </div>
         </section>
