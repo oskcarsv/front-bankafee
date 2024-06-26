1b1459a9e8b3fe4171c36cc17a9bf96e342d62b6
amendez2019362 <amendez-2019362@kinal.edu.gt>
2024-06-25 23:51:25 -0600

    fix: Update link in InfoService component to navigate to correct page

--- src/components/serviceCategory/InfoService.jsx
+++ src/components/serviceCategory/InfoService.jsx
@@ -33,7 +33,7 @@ export const InfoService = () => {
                             <button className='btn-discount'>see discount</button>
                         </div>
                         <div className='content-input-service'>
-                            <Link to="/productCategory"><button className='btn-discount-back'>Back</button></Link>
+                            <Link to="/serviceCategory"><button className='btn-discount-back'>Back</button></Link>
                         </div>
                     </form>
                 </div>
