c5fce721dbdf4dffc2ab8da67de260cff2e50ca6
amendez2019362 <amendez-2019362@kinal.edu.gt>
2024-06-26 18:56:55 -0600

    refactor: Update GeneralHistory component structure and styles

--- src/components/historyUser/GeneralHistory.jsx
+++ src/components/historyUser/GeneralHistory.jsx
@@ -7,24 +7,33 @@ export const GeneralHistory = () => {
                 <h1 className="title-history">Last movements</h1>
             </div>
             <div className="content-info-table">
-                <div>
-                    <table className="table-title">
-                        <tr className="table-header">
-                            <th className="title-info">Deposit of</th>
-                            <th className="title-info">No. Account</th>
-                            <th className="title-info">Date and Time</th>
+                <table className="table-generali">
+                    <thead className='thead-titles-generali'>
+                        <tr>
+                            <th>Deposit of</th>
+                            <th>No. Account</th>
+                            <th>Date and Time</th>
                         </tr>
-                    </table>
-                </div>
-                <table className="table-content">
-                    <tr className="table-body">
-                        <td className="info-table-body">GTQ 100.00 to Angel</td>
-                        <td className="info-table-body">54121 21020 1211</td>
-                        <td className="info-table-body">2024/12/25 - 5:55 pm</td>
-                    </tr>
+                    </thead>
+                    <tbody>
+                        <tr className="table-body">
+                            <td className="info-table-body">GTQ 100.00 to Angel</td>
+                            <td className="info-table-body">54121 21020 1211</td>
+                            <td className="info-table-body">2024/12/25 - 5:55 pm</td>
+                        </tr>
+                        <tr className="table-body">
+                            <td className="info-table-body">GTQ 550.00 to Chino</td>
+                            <td className="info-table-body">507821 21020 1211</td>
+                            <td className="info-table-body">2024/01/01 - 8:55 am</td>
+                        </tr>
+                        <tr className="table-body">
+                            <td className="info-table-body">GTQ 15.00 to Revo</td>
+                            <td className="info-table-body">023621 21020 1211</td>
+                            <td className="info-table-body">2023/12/31 - 11:00 am</td>
+                        </tr>
+                    </tbody>
                 </table>
             </div>
-            <hr />
         </section >
     );
 }
