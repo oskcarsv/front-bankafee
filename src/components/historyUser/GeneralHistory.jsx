38992737490ff29e730460d7d1355bcd6ac6b62e
Jose David Soto Puac <jsoto-2019315@kinal.edu.gt>
2024-07-03 19:52:55 -0600

    chore: Update class names in GeneralHistory component and remove unused CSS files

--- src/components/historyUser/GeneralHistory.jsx
+++ src/components/historyUser/GeneralHistory.jsx
@@ -7,21 +7,31 @@ export const GeneralHistory = () => {
         <h1 className="title-history">Last movements</h1>
       </div>
       <div className="content-info-table">
-        <div>
-          <table className="table-title">
-            <tr className="table-header">
-              <th className="title-info">Deposit of</th>
-              <th className="title-info">No. Account</th>
-              <th className="title-info">Date and Time</th>
+        <table className="table-recent-activity-homepage">
+          <thead className="recent-activity-title"> 
+            <tr>
+              <th>Deposit of</th>
+              <th className="noAccount">No. Account</th>
+              <th>Date and Time</th>
             </tr>
-          </table>
-        </div>
-        <table className="table-content">
-          <tr className="table-body">
-            <td className="info-table-body">GTQ 100.00 to Angel</td>
-            <td className="info-table-body">54121 21020 1211</td>
-            <td className="info-table-body">2024/12/25 - 5:55 pm</td>
-          </tr>
+          </thead>
+          <tbody>
+            <tr className="recent-activity-info">
+              <td className="info-recent-activity">GTQ 100.00 to Angel</td>
+              <td className="info-recent-activity noAccount">54121 21020 1211</td>
+              <td className="info-recent-activity">2024/12/25 - 5:55 pm</td>
+            </tr>
+            <tr className="recent-activity-info">
+              <td className="info-recent-activity">GTQ 100.00 to Angel</td>
+              <td className="info-recent-activity noAccount">54121 21020 1211</td>
+              <td className="info-recent-activity">2024/12/25 - 5:55 pm</td>
+            </tr>
+            <tr className="recent-activity-info">
+              <td className="info-recent-activity">GTQ 100.00 to Angel</td>
+              <td className="info-recent-activity noAccount">54121 21020 1211</td>
+              <td className="info-recent-activity">2024/12/25 - 5:55 pm</td>
+            </tr>
+          </tbody>
         </table>
       </div>
       <hr />
--- src/pages/historyUser/historyUser.jsx
+++ src/pages/historyUser/historyUser.jsx
@@ -3,19 +3,19 @@ import { MessageHistory } from "../../components/historyUser/MessageHistory.jsx"
 import { ListHistoryUser } from "../../components/historyUser/ListHistoryUser.jsx";
 import { GeneralHistory } from "../../components/historyUser/GeneralHistory.jsx";
 
-import "../../pages/historyUser/historyUser.css";
+// import "../../pages/historyUser/historyUser.css";
 
 export const HistoryUser = () => {
   return (
-    <>
-      <div className="sidebar-history">
-        <Sidebar />
-      </div>
-      <div className="content-history-view">
-        <MessageHistory />
-        <ListHistoryUser />
-        <GeneralHistory />
-      </div>
-    </>
+      <main className="main-container-home">
+        <nav className="sidebar-container">
+          <Sidebar />
+        </nav>
+        <article className="home-page-container">
+          <MessageHistory />
+          <ListHistoryUser />
+          <GeneralHistory />
+        </article>
+      </main>
   );
 };
