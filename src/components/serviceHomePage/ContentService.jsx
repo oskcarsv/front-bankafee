d6e823c8301f17550efd80b7a8994deb80bb4ce9
Jose David Soto Puac <jsoto-2019315@kinal.edu.gt>
2024-07-04 02:03:24 -0600

    chore: Update serviceHomePage layout and class names

---
+++ src/components/serviceHomePage/ContentService.jsx
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom";
+
+import logoPizzaHot from "../../assets/iconServicesUser/logoPizzaHut.svg";
+
+export const ContentService = () => {
+    return (
+        <Link to="/serviceCategory" className="link-service">
+            <div className="card-service">
+                <img src={logoPizzaHot} alt="img" className="img-service-user" />
+                <p className="title-service-user">
+                    Descuento en Pizza Hot de un 50%{" "}
+                </p>
+            </div>
+        </Link>
+    );
+};
--- src/components/serviceHomePage/Services.jsx
+++ src/components/serviceHomePage/Services.jsx
@@ -1,6 +1,4 @@
-import { Link } from "react-router-dom";
-
-import logoPizzaHot from "../../assets/iconServicesUser/logoPizzaHut.svg";
+import { ContentService } from "./ContentService";
 
 import "../../styles/serviceHomPage/services.css";
 
@@ -8,38 +6,10 @@ export const Services = () => {
   return (
     <section className="section-service-page">
       <div className="content-service-page">
-        <Link to="/serviceCategory" className="link-service">
-          <div className="card-service">
-            <img src={logoPizzaHot} alt="img" className="img-service-user" />
-            <p className="title-service-user">
-              Descuento en Pizza Hot de un 50%{" "}
-            </p>
-          </div>
-        </Link>
-        <Link to="/" className="link-service">
-          <div className="card-service">
-            <img src={logoPizzaHot} alt="img" className="img-service-user" />
-            <p className="title-service-user">
-              Descuento en Pizza Hot de un 50%{" "}
-            </p>
-          </div>
-        </Link>
-        <Link to="/" className="link-service">
-          <div className="card-service">
-            <img src={logoPizzaHot} alt="img" className="img-service-user" />
-            <p className="title-service-user">
-              Descuento en Pizza Hot de un 50%{" "}
-            </p>
-          </div>
-        </Link>
-        <Link to="/" className="link-service">
-          <div className="card-service">
-            <img src={logoPizzaHot} alt="img" className="img-service-user" />
-            <p className="title-service-user">
-              Descuento en Pizza Hot de un 50%{" "}
-            </p>
-          </div>
-        </Link>
+        <ContentService />
+        <ContentService />
+        <ContentService />
+        <ContentService />
       </div>
     </section>
   );
