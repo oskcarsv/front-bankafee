14aba32b6bf8f026a417fd13f35cf62ccdb667ca
Jose David Soto Puac <jsoto-2019315@kinal.edu.gt>
2024-07-04 01:44:29 -0600

    chore: Refactor ProductCategory component and add ContentProductCategory subcomponent

--- src/components/productCategory/ProductCategory.jsx
+++ src/components/productCategory/ProductCategory.jsx
@@ -1,6 +1,5 @@
 import { Link } from "react-router-dom";
-
-import computadora from "../../assets/iconsProduct/computadora.svg";
+import { ContentProductCategory } from "./ContentProductCategory";
 
 import "../../styles/productCategory/productCategory.css";
 
@@ -8,42 +7,10 @@ export const ProductCategoryAll = () => {
   return (
     <section className="section-product-category">
       <div className="content-product-category">
-        <Link to="/purchaseForm" className="link-category">
-          <div className="card-product">
-            <img src={computadora} alt="img" className="img-product-category" />
-            <p className="text-info-product">
-              Laptop Huawei Core i5, 13", 16GB RAM, 512GB SSD
-            </p>
-            <p className="text-info-product">Q 16,999.00</p>
-          </div>
-        </Link>
-        <Link to="/purchaseForm" className="link-category">
-          <div className="card-product">
-            <img src={computadora} alt="img" className="img-product-category" />
-            <p className="text-info-product">
-              Laptop Huawei Core i5, 13", 16GB RAM, 512GB SSD
-            </p>
-            <p className="text-info-product">Q 16,999.00</p>
-          </div>
-        </Link>
-        <Link to="/purchaseForm" className="link-category">
-          <div className="card-product">
-            <img src={computadora} alt="img" className="img-product-category" />
-            <p className="text-info-product">
-              Laptop Huawei Core i5, 13", 16GB RAM, 512GB SSD
-            </p>
-            <p className="text-info-product">Q 16,999.00</p>
-          </div>
-        </Link>
-        <Link to="/purchaseForm" className="link-category">
-          <div className="card-product">
-            <img src={computadora} alt="img" className="img-product-category" />
-            <p className="text-info-product">
-              Laptop Huawei Core i5, 13", 16GB RAM, 512GB SSD
-            </p>
-            <p className="text-info-product">Q 16,999.00</p>
-          </div>
-        </Link>
+        <ContentProductCategory />
+        <ContentProductCategory />
+        <ContentProductCategory />
+        <ContentProductCategory />
       </div>
     </section>
   );
